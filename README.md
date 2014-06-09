4437dd1d9211fc066736bfda17425550794fca12
Schneider, Bill <SchneidB@advisory.com>
2014-06-09 13:38:30 -0400

    initial checkin

---
+++ README.md
@@ -0,0 +1,5 @@
+Buzzword Bingo Generator
+=========
+To use this, point your browser at bingo.html directly.  This all runs client
+side with no server required.  
+Print with normal browser print capability.
---
+++ bingo.js
@@ -0,0 +1,64 @@
+$(document).ready(function() { 
+	var generateCards = function generateCards() {
+		var words, num, i, randomizedWords;
+		
+		words = $("textarea").val().split("\n");
+		_.remove(words, function(word) { return !word; });
+		while (words.length < 24) {
+			words = words.concat(words);
+		}
+		
+		num = $("input[name='count']").val();
+		$("#output").empty();
+		
+		for (i = 0; i < num; i++) {
+			randomizedWords = randomizeCardWords(words);
+			console.debug(randomizedWords);
+			$("#output").append(generateOneCard(randomizedWords));
+		}
+		return false;
+	};
+	
+	var randomizeCardWords = function randomizeCardWords(words) {
+		words = _.shuffle(words);
+		var matrix = [];
+		var i, j;
+		
+		for (i = 0; i < 5; i++) {
+			if (!matrix[i]) {
+				matrix[i] = [];
+			}
+		
+			for (j = 0; j < 5; j++) {
+				if (i == 2 && j == 2) {
+					matrix[i].push("*");
+					continue;
+				}
+				matrix[i].push(words.pop());
+			}
+		}
+		return matrix;
+	};
+	
+	var generateOneCard = function generateOneCard(matrix) {
+		var $table = $("<table class='table table-bordered'><tr></tr></table>"), 
+			$tr, $td;
+		var i, j;
+		for (i = 0; i < 5; i++) {
+			$table.find("tr").append($("<th>" + "BINGO".charAt(i) + "</th>"));
+		}
+			
+		for (i = 0; i < 5; i++) {
+			$tr = $("<tr>")	
+			for (j = 0; j < 5; j++) { 
+				$td = $("<td>" + matrix[i][j] + "</td>");
+				$tr.append($td);
+			}
+			$table.append($tr);
+		}
+		return $table;
+	};
+	
+	$("#generate").click(generateCards);
+
+});
